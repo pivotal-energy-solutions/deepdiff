90491eec6dceb6645a551734091aa4d9d34008a7
Steven Klass <rh0dium@users.noreply.github.com>
2018-04-23 07:50:23 -0700

    Updated Version info

--- deepdiff/__init__.py
+++ deepdiff/__init__.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
 from .deepdiff import DeepDiff
+
+__name__ = 'deepdiff'
+__author__ = 'Seperman'
+__version_info__ = (0, 6, 2)
+__version__ = '.'.join(map(str, __version_info__))
+__date__ = '2018/04/20 4:47:00 PM'
+__credits__ = ['Seperman', 'Steven Klass']
+__license__ = 'See the file LICENSE.txt for licensing information.'
